589b373666dfa60199f52f2c314e7da43fc6c24f
javierespinoza09 <javierespriv44@gmail.com>
2024-01-23 13:30:27 -0600

    Update

--- src/Agent.svh
+++ src/Agent.svh
@@ -1,4 +1,10 @@
-class Agent extends  /* base class*/;
+class Agent extends  uvm_agent;
+	`uvm_component_utils(Agent);
+	string name = "Agent"
+	function new(string name, uvm_component parent = null);
+	    super.new(name,parent);
+	 endfunction
+
 	
 endclass : Agent
 
