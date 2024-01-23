a19190921c647e2551b668fb74f601ede65f5af4
javierespinoza09 <99993066+javierespinoza09@users.noreply.github.com>
2024-01-22 21:25:29 -0600

    Se incluye RTL y MEM en wrapper, se plantea if (#1)

--- src/RTL_MEM_Wrapper.svh
+++ src/RTL_MEM_Wrapper.svh
@@ -8,12 +8,42 @@
 
 `include "../../Verificacion_RISCV_TEC/TEC_RISCV/TOP/topcore_tecriscv.sv"
 `include "../../Verificacion_RISCV_TEC/test_env/core_spi_uart/IS25WP032D.v"
+`include "wrapper_if.svh"
 
-module RTL_MEM_Wrapper (
-	input clk,    // Clock
-	input clk_en, // Clock Enable
-	input rst_n,  // Asynchronous reset active low
-	
-);
+
+module RTL_MEM_Wrapper ( wrapper_if _if);
+	wire MISO;
+	logic RX_UART;
+	wire MOSI;
+	wire SCLK;
+	wire SCS;
+	wire TX_UART;
+
+	topcore_tecriscv uut(
+	.clk(_if.clk),
+	.reset(_if.reset),
+	.MISO(MISO),
+	.RX_UART(RX_UART),
+	.maip(_if.maip),
+	.MOSI(MOSI),
+	.SCLK(SCLK),
+	.SCS(SCS),
+	.TX_UART(TX_UART),
+	.full_range_level_shifter(_if.full_range_level_shifter),
+	.IS_Val(_if.IS_Val),
+	.IS_Config(_if.IS_Config),
+	.IS_Trigger(_if.IS_Trigger),
+	.Reg_GPIO_en(_if.Reg_GPIO_en),
+	.Reg_GPIO_int(_if.Reg_GPIO_int),
+	.Reg_GPIO_out(_if.Reg_GPIO_out)
+	);
+
+	IS25WP032D mem(
+		.SCLK(SCLK),
+		.CS(SCS),
+		.SI(MOSI),
+		.SO(MISO),
+		.WP(1'b1),
+		.SIO3(1'b1));
 
 endmodule : RTL_MEM_Wrapper
---
+++ src/wrapper_if.svh
@@ -0,0 +1,22 @@
+//////////////////////////////////////////
+//	Interface: wrapper_if				//
+//	Interface que permite comunicar el	//
+//	entorno de prueba con el RTL		//
+//	Programador: Javier Espinoza Rivera	//
+// 	Supervisor: Roberto Molina 			//
+//////////////////////////////////////////
+
+interface wrapper_if;
+	logic clk;
+	logic reset;
+	logic  [7:0] gpio;
+	logic [7:0] full_range_level_shifter;
+	logic [31:0] IS_Val;
+	logic [31:0] IS_Config;
+	logic [3:0] IS_Trigger;
+	logic maip;
+	logic [7:0] Reg_GPIO_en;
+	logic [7:0] Reg_GPIO_int;
+	logic [7:0] Reg_GPIO_out;
+	
+endinterface : wrapper_if
